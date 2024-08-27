768244554fc19fcba389a2892d5f623bde6d9486
Karthikindeppa <149152160+Karthikindeppa@users.noreply.github.com>
2024-08-27 11:07:27 +0530

    Add files via upload

---
+++ Tarun pro 12.py
@@ -0,0 +1,23 @@
+import time
+start=time.time()
+from queue import Queue
+graph=
+{'A':['B','D','E','F'],'D':['A'],'B':['A','F','C',],'F':['B','A'],'C':['B'],'E':['A']}
+print("given graph is:")
+def BFS(input_graph,source):
+    Q=Queue()
+    visited_vertices=list()
+    Q.put(source)
+    visited_vertices.append(source)
+    while not Q.empty():
+        vertex=Q.get()
+        print(vertex,end="")
+        for u in input_graph[vertex]:
+            if u not in(visited_vertices):
+                Q.put(u)
+                visited_vertices.append(u)
+print("BFS traversal of graph with source A is:")
+BFS(graph,"A")
+end=time.time()
+print(f"runtime of the program is{end-start}")
+                            
